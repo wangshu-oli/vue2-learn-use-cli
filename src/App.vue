afa0cdc40fe74bb8a5ec3d6b5c61a7d97dd49d9a
Oliver <xiaojian.zhang@nx-engine.com>
2022-07-09 14:34:42 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 结构 -->
+  <div>
+    <School></School>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*交互内容*/
+// 在脚手架里.vue后缀可以省略
+import School from "./components/School";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    School
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/School.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 结构 -->
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+<script>
+/*交互内容*/
+export default {
+  name: 'ASchool',
+  data() {
+    return {
+      name: 'xx大学',
+      address: '北京'
+    }
+  }
+}
+</script>
+<style>
+</style>
